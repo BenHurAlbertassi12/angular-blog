2d2d7ba6d8c26f438275c7b1b3a1cf74d03f3de8
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-02 17:19:12 -0300

    Blog Final

---
+++ src/app/data/fakeData.ts
@@ -0,0 +1,87 @@
+export const fakeData = [
+  {
+    id: '1',
+    title: 'Roteiro Inesquecível em Campos do Jordão',
+    description: `
+Um Roteiro Inesquecível em Campos do Jordão – SP. Cinco dias de experiências gastronômicas e turísticas.
+
+Quem busca um destino para aproveitar o friozinho da serra sem sair do Brasil, encontra em Campos do Jordão a combinação perfeita de clima ameno, gastronomia requintada e charmosas construções com estilo alpino. A cidade oferece um clima agradável, paisagens naturais exuberantes, arquitetura europeia,
+
+Neste guia completo de um roteiro inesquecível em Campos do Jordão, vamos explorar tudo o que você precisa saber para aproveitar seus 5 dias em Campos do Jordão, incluindo dicas de hospedagem, gastronomia, comidas típicas e passeios.
+
+Hospedagem aconchegante em Campos do Jordão
+Para aproveitar ao máximo sua viagem, é importante escolher uma hospedagem que atenda às suas necessidades. Em Campos do Jordão, existem diversas opções de hospedagem, desde hotéis luxuosos até pousadas mais simples e aconchegantes.
+
+Se você procura por uma opção de hospedagem luxuosa, o Hotel Toriba é uma excelente escolha, com uma bela arquitetura em estilo europeu, spa e uma vista privilegiada para as montanhas.
+
+Outra opção é o Hotel Serra da Estrela, com quartos confortáveis, piscina aquecida e uma localização privilegiada, próximo ao centrinho da cidade. Para quem busca uma opção mais acessível, a Pousada Annecy é uma excelente escolha, com quartos aconchegantes e uma bela vista para as montanhas.
+
+Gastronomia de dar água na boca em Campos do Jordão
+Se você é fã de fondues, o restaurante Baden Baden é uma parada obrigatória, com opções de fondue de queijo, carne e chocolate.
+
+Outra opção é o restaurante Vila Chã, que oferece pratos da culinária portuguesa e uma bela vista para as montanhas.
+
+Para quem gosta de comida italiana, a Cantina Di Nonna Mimi é uma excelente escolha, com massas artesanais e um ambiente aconchegante. Já para quem quer experimentar as comidas típicas da região, o restaurante Krokodillo é uma excelente escolha, com opções de trutas, pinhão e outras iguarias locais.    
+
+Comidas típicas especias de Campos do Jordão
+Além dos restaurantes que oferecem comida típica da região, Campos do Jordão é conhecida por seus produtos locais, como o pinhão e a truta. O pinhão é um fruto típico da região e pode ser encontrado em diversos restaurantes, em pratos como risotos, sopas e saladas. Já a truta é criada em pisciculturas locais e pode ser encontrada em diversos pratos, como grelhada, frita ou empanada.
+
+Passeios com divesas opções em Campos do Jordão
+Campos do Jordão oferece diversas opções de passeios, desde caminhadas em trilhas na natureza até visitas a museus e parques. Se você estiver procurando por uma experiência incrível, não perca a chance de fazer o passeio de teleférico até o Morro do Elefante e apreciar a vista panorâmica da cidade e das montanhas.
+
+Outra opção é o Parque Amantikir, um belo jardim com diversas espécies de plantas e paisagens incríveis. O Museu Felícia Leirner oferece uma experiência única para os amantes da arte e da natureza, com um acervo de esculturas ao ar livre em um ambiente deslumbrante. Para quem gosta de aventura, o Parque Tarundu é uma excelente escolha, com opções de tirolesa, arvorismo e paintball.
+
+Roteiro Inesquecível de 5 dias em Campos do Jordão
+
+Dia 1:
+No primeiro dia em Campos do Jordão, recomendamos visitar o Parque Amantikir pela manhã, para aproveitar o frescor da natureza. Em seguida, almoce no restaurante Vila Chã, que fica próximo ao parque e oferece pratos deliciosos da culinária portuguesa.
+
+À tarde, faça um passeio de teleférico até o Morro do Elefante, onde é possível ver uma vista incrível da cidade e das montanhas. Para o jantar, experimente o fondue de queijo no restaurante Baden Baden.
+
+Dia 2:
+No segundo dia, comece o dia com um café da manhã na Pousada Annecy, que oferece um delicioso café da manhã com produtos locais. Em seguida, faça uma caminhada pela trilha do Bauzinho, que oferece uma vista incrível da cidade e das montanhas.
+
+Para o almoço, experimente o famoso prato de truta no restaurante Krokodillo. À tarde, visite o Museu Felícia Leirner, que oferece um acervo incrível de esculturas em meio à natureza. Para o jantar, experimente as massas artesanais na Cantina Di Nonna Mimi.
+
+Dia 3:
+No terceiro dia, comece o dia com um café da manhã no Hotel Toriba, que oferece um café da manhã completo e uma vista incrível das montanhas. Em seguida, faça um passeio pela Vila Capivari, onde é possível encontrar diversas lojas e restaurantes charmosos.
+
+Para o almoço, experimente o prato de truta no restaurante Truta & Cia. À tarde, visite o Parque Tarundu e experimente as opções de aventura, como tirolesa e arvorismo. Para o jantar, experimente as opções de fondue no restaurante Safari.
+
+Dia 4:
+No quarto dia, comece o dia com um café da manhã no Hotel Serra da Estrela, que oferece um café da manhã delicioso com uma vista incrível das montanhas. Em seguida, faça um passeio de trem até a cidade vizinha de Santo Antônio do Pinhal, onde é possível visitar o Mirante do Cruzeiro e a Cachoeira do Lageado.
+
+Para o almoço, experimente o famoso arroz com feijão tropeiro no restaurante Churrasco ao Vivo. À tarde, volte para Campos do Jordão e visite o Palácio Boa Vista, uma bela construção em estilo europeu. Para o jantar, experimente as opções de fondue no restaurante Engenho do Pão.
+
+Dia 5:
+No último dia em Campos do Jordão, comece o dia com um café da manhã na Pousada do Conde, que oferece um café da manhã delicioso em um ambiente aconchegante. Uma visita ao Parque Estadual de Campos do Jordão é uma ótima ideia para quem quer se desconectar da cidade e passar um tempo em meio à natureza, desfrutando de trilhas desafiadoras e refrescantes cachoeiras.
+
+Para o almoço, experimente o prato de truta no restaurante Araucária. Depois, à tarde, faça um passeio de cavalo pelo Haras Polana, que oferece uma vista incrível das montanhas. Para o jantar, por sua vez, experimente os pratos sofisticados no restaurante Charpentier.
+
+Roteiro Inesquecível em Campos do Jordão: Em resumo.
+Campos do Jordão é conhecida por abrigar importantes eventos culturais. Além disso, a cidade é encantadora e combina paisagens naturais deslumbrantes com arquitetura elegante.
+
+Além disso, Campos do Jordão oferece diversas opções de lazer e entretenimento, tornando-se um lugar ideal para quem busca descanso, tranquilidade, cultura e diversão.
+
+By, Lea`,
+    photoCover:
+      'https://legalpraviajar.com/wp-content/uploads/2023/04/brazil-g9952fb7f6_1920-1024x576.jpg',
+  },
+  {
+    id: '2',
+    title: 'Lorem Ipsum',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae semper neque, quis feugiat velit. Donec tristique, diam a congue fermentum, erat ex tincidunt lorem, nec consequat nibh ex et odio. Nulla mauris velit, hendrerit sed enim sit amet, iaculis semper nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla euismod hendrerit libero, et scelerisque neque. Nulla vehicula viverra justo, at rutrum lacus volutpat ac. Aliquam eu imperdiet massa, in sollicitudin velit. Integer lacinia ante metus, sed tristique justo porta dapibus. Suspendisse in ligula mi. Phasellus rutrum libero ante, a pellentesque enim fringilla id. Morbi ac dictum tortor. Vivamus cursus vulputate diam, accumsan bibendum purus faucibus et. Morbi maximus urna non ultricies cursus. Etiam nec mi condimentum, tincidunt lorem vel, accumsan enim.
+
+Nam at dui turpis. Curabitur tortor felis, sagittis vel neque eget, feugiat pharetra ipsum. Nulla eget sapien vitae massa facilisis elementum eget vel dolor. Fusce interdum tempor condimentum. Vestibulum orci risus, lobortis a pretium eu, porttitor ac magna. Pellentesque interdum vestibulum nunc ac fringilla. Quisque lacus erat, facilisis congue eros a, cursus egestas diam. Mauris sodales quam enim, a congue felis pretium et. Phasellus ornare dui non magna sagittis, finibus varius nunc efficitur. Donec sit amet est convallis, blandit neque condimentum, fringilla arcu.
+
+Maecenas gravida dignissim nulla ac dapibus. Sed in orci eu dolor varius vestibulum quis condimentum magna. Vestibulum justo quam, convallis dictum congue nec, tempor quis mi. Nulla maximus, est vitae auctor volutpat, enim est rhoncus erat, non viverra felis mi quis felis. Vivamus placerat lobortis dui. Fusce dignissim scelerisque nulla, ac dictum sem viverra at. Curabitur pulvinar eget ante sit amet suscipit. Nam ac augue interdum, iaculis diam eu, placerat est. Nam volutpat enim mauris, sit amet tempus tellus sollicitudin egestas. Nullam elementum nisl sit amet porttitor malesuada. Praesent hendrerit orci in ultricies iaculis. Fusce fringilla metus ut turpis scelerisque, sit amet euismod ligula varius. Nullam sed euismod ex, condimentum sodales mi. Phasellus porttitor leo quis laoreet congue.
+
+Etiam sodales hendrerit felis a varius. Duis ac augue metus. Ut non est ac massa finibus malesuada vel at augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin et suscipit justo. Morbi id lorem aliquam, ultricies odio at, varius massa. Pellentesque convallis orci ut arcu tristique, in facilisis mi interdum. Integer nulla lacus, posuere blandit laoreet volutpat, efficitur vel arcu. In hac habitasse platea dictumst. Curabitur porta rutrum vestibulum. Duis in risus eu ex suscipit fermentum. Proin in nisl placerat, ornare erat eu, eleifend nulla. Ut nec nulla egestas, semper massa lobortis, eleifend lectus. Sed sit amet purus id ligula semper pulvinar at sit amet massa. Cras iaculis lectus sit amet nibh tincidunt, eget consectetur urna mollis. Morbi urna quam, commodo ac sem sit amet, aliquet ullamcorper enim.
+
+Mauris cursus orci justo, eu porta diam porta in. Nulla facilisi. Cras gravida sapien in hendrerit lacinia. Vestibulum molestie diam eleifend feugiat porttitor. Vivamus id velit sagittis mauris dapibus finibus non nec nisl. Donec eu orci ullamcorper, aliquam libero eu, lacinia diam. Sed commodo congue urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam eget erat accumsan massa pellentesque sodales eget sit amet sapien. Nullam rhoncus eu augue vitae fermentum. Sed cursus eros eget elit porttitor rhoncus.
+
+Generated 5 paragraphs, 539 words, 3620 bytes of Lorem Ipsum`,
+    photoCover:
+      'https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/05/luffy-one-piece.webp?fit=1200%2C675&ssl=1',
+  },
+];
